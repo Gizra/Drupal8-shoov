3c9fc0c05ed4648f897c6ebd952ff6909d324cac
DavidKohav <david@gizra.com>
2015-09-29 11:56:38 +0300

    Add a visual monitoring.

---
+++ visual-monitor/test/tests.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var shoovWebdrivercss = require('shoov-webdrivercss');
+var projectName = 'Drupal 8'
+// This can be executed by passing the environment argument like this:
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=chrome mocha
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=ie11 mocha
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=iphone5 mocha
+
+var capsConfig = {
+  'chrome': {
+    project: projectName,
+    'browser' : 'Chrome',
+    'browser_version' : '42.0',
+    'os' : 'OS X',
+    'os_version' : 'Yosemite',
+    'resolution' : '1024x768'
+  },
+  'ie11': {
+    project: projectName,
+    'browser' : 'IE',
+    'browser_version' : '11.0',
+    'os' : 'Windows',
+    'os_version' : '7',
+    'resolution' : '1024x768'
+  }
+};
+
+var selectedCaps = process.env.SELECTED_CAPS || undefined;
+var caps = selectedCaps ? capsConfig[selectedCaps] : undefined;
+
+var providerPrefix = process.env.PROVIDER_PREFIX ? process.env.PROVIDER_PREFIX + '-' : '';
+var testName = selectedCaps ? providerPrefix + selectedCaps : providerPrefix + 'default';
+
+var baseUrl = process.env.BASE_URL ? process.env.BASE_URL : 'http://d8testtheuiforjs2arytcuouk.devcloud.acquia-sites.com/';
+
+var resultsCallback = process.env.DEBUG ? console.log : shoovWebdrivercss.processResults;
+
+describe('Visual monitor testing', function() {
+
+  this.timeout(99999999);
+  var client = {};
+
+  before(function(done){
+    client = shoovWebdrivercss.before(done, caps);
+  });
+
+  after(function(done) {
+    shoovWebdrivercss.after(done);
+  });
+
+  it('should show the home page',function(done) {
+    client
+      .url(baseUrl)
+      .webdrivercss(testName + '.homepage', {
+        name: '1',
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+});
